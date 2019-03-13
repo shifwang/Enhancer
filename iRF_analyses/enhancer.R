37ddb9f9548bd81d15e8a95b7ed77752156b9dc0
Yu Wang <shifwangonline@gmail.com>
2019-03-12 23:59:02 -0700

    update the iRF result for enhancer

--- iRF_analyses/enhancer.R
+++ iRF_analyses/enhancer.R
@@ -1,19 +1,19 @@
 library(iRF)
 n.cores <- 24
 set.seed(1)
-load('./data/enhancer.Rdata')
+load('./enhancer.Rdata')
 
 rit.params <- list(depth=5, nchild=2, ntree=500, class.id=1, class.cut=NULL)
 fit <-  iRF(x=X[train.id,], 
            y=as.factor(Y[train.id]), 
            xtest=X[test.id,], 
            ytest=as.factor(Y[test.id]), 
-           n.iter=4,
-           interactions.return=1:4, 
+           n.iter=1,
+           interactions.return=c(1), 
            rit.params=rit.params,
            varnames.grp=varnames.all$Predictor_collapsed, 
            n.core=n.cores, 
-           n.bootstrap=30,
+           n.bootstrap=1,
            rit.param=rit.params
           )
 save(file='results_enhancer.Rdata', fit)
